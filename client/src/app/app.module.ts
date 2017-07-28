754576792c7861662ac833b076384b5b03e8ae3f
saguo <saguo@umich.edu>
2017-07-27 17:28:45 -0700

    add tasks

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { TasksComponent} from './tasks/tasks.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent TasksComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ client/src/app/tasks/tasks.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  //moduleId: module.id,
+  selector: 'tasks',
+  template: `<p>Tasks<p>`,
+})
+export class TasksComponent { }
